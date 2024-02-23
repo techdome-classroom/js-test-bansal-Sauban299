a17bad34ab258c2c0075c4494b14c4c09880b28b
Sauban Ahmad Wahib <114351487+Sauban299@users.noreply.github.com>
2024-02-23 13:05:01 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,11 +1,21 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    
-};
+function isValid(s) {
+    const stack = [];
+    const mapping = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
 
-module.exports = { isValid };
+    for (let char of s) {
+        if (char in mapping) {
+            const topElement = stack.pop();
+            if (mapping[char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
 
-
+    return stack.length === 0;
+}
